fbec8fbe3331f4ad2096c2b4675eb816ba833623
shivanand4761 <110733118+shivanand4761@users.noreply.github.com>
2022-09-27 11:43:03 +0530

    Update ec2.tf

--- ec2-instance/ec2.tf
+++ ec2-instance/ec2.tf
@@ -10,7 +10,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-
 resource "aws_instance" "machine1" {
   ami                         = "ami-0f924dc71d44d23e2"
   instance_type               = "t2.medium"
